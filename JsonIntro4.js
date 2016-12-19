7f0e5994c34307eb4058396865312b017749bb0b
Hrishikesh Kadam <Hrishikesh Kadam>
2016-12-19 11:38:42 +0530

    My initial commit into local repository

---
+++ JsonIntro4.js
@@ -0,0 +1,290 @@
+
+var data = {
+  "success": [
+    {
+      "account_Name": "3E Company",
+      "accountID": "0018000000ho63dAAA",
+      "accountDetails": {
+        "amount": "105700",
+        "alerts": 0,
+        "opportunities": 3,
+        "support_request": 0,
+        "insights": "0",
+        "agenda": 3
+      },
+      "address": "1905 Aston Ave Ste 100, Carlsbad, UNITED STATES,CA,null",
+      "zipCode": "92008",
+      "latitude ": "33.130923",
+      "longitude": "-117.287764"
+    },
+    {
+      "account_Name": "ADP, Inc. - MAIN",
+      "accountID": "0018000000hoApcAAE",
+      "address": "1 ADP Blvd, Roseland, UNITED STATES,NJ,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": null,
+        "support_request": "undefined",
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "7068",
+      "latitude ": null,
+      "longitude": null
+    },
+    {
+      "account_Name": "API Technologies Corp",
+      "accountID": "0013400001LJbbFAAT",
+      "address": "8031 Avonia Rd, Fairview, UNITED STATES,PA,null",
+      "zipCode": "16415-2829",
+      "latitude ": "42.0045659",
+      "longitude": "-80.23526199999999"
+    },
+    {
+      "account_Name": "API Technologies Corp",
+      "accountID": "0013400001LLwQJAA1",
+      "address": "4705 S Apopportunitieska Vineland Rd, Orlando, UNITED STATES,FL,null",
+      "accountDetails": null,
+      "zipCode": "32819-3151",
+      "latitude ": "28.4927777",
+      "longitude": "-81.5078783"
+    },
+    {
+      "account_Name": "Aetna Inc (use this one)",
+      "accountID": "0018000000hoBCjAamount",
+      "address": "151 Farmington Ave, Hartford, UNITED STATES,CT,null",
+      "accountDetails": {
+        "amount": "2423",
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "latitude ": "41.76678099999999",
+      "longitude": "-72.689567"
+    },
+    {
+      "account_Name": "account_Namelertsog Devices Internationalerts Inc",
+      "accountID": "0018000000hoF9tAAE",
+      "address": "1 Technology Way, Norwood, UNITED STATES,MA,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": null,
+        "insights": "0",
+        "agenda": 3
+      },
+      "latitude ": "42.210942",
+      "longitude": "-71.180871"
+    },
+    {
+      "account_Name": "Architecturalerts Energy Corporation",
+      "accountID": "0018000000xaJ68AAE",
+      "address": "2540 Frontier Ave, Boulder, UNITED STATES,CO,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": null,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "80301-2400",
+      "latitude ": "40.0235403",
+      "longitude": "-105.2467779"
+    },
+    {
+      "account_Name": "Arrowstreet Capitalerts Limited Partnership",
+      "accountID": "0018000000hoLsjAAE",
+      "address": "144 Brattle St, Camountbridge, UNITED STATES,MA,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "2138",
+      "latitude ": "42.377346",
+      "longitude": "-71.1311769"
+    },
+    {
+      "account_Name": "Atlantic Inertialerts Systems",
+      "accountID": "0018000000hnzuxAAA",
+      "address": "250 Knotter Dr, Cheshire, UNITED STATES,CT,null",
+      "accountDetails": {
+        "amount": "2423",
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "latitude ": "41.55108370000001",
+      "longitude": "-72.916978"
+    },
+    {
+      "account_Name": "Atlantic Inertialerts Systems",
+      "accountID": "0018000000hoG2fAAE",
+      "address": "801 Irving Wick Dr W, Heath, UNITED STATES,OH,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0"
+      },
+      "zipCode": "43056",
+      "latitude ": "40.0246175",
+      "longitude": "-82.47285839999999"
+    },
+    {
+      "account_Name": "Austalerts USA",
+      "accountID": "0018000000ho8USAAY",
+      "address": "100 Dunlap Drive, Mobile, UNITED STATES,alerts,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "36633",
+      "latitude ": "30.693317",
+      "longitude": "-88.031571"
+    },
+    {
+      "account_Name": "Automated Logic",
+      "accountID": "00180000010pUZMAA2",
+      "address": "307 Pomona Dr, Greensboro, UNITED STATES,NC,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "27407-1653",
+      "latitude ": "36.0658638",
+      "longitude": "-79.864631"
+    },
+    {
+      "account_Name": "Automated Logic Corporation",
+      "accountID": "0018000000hoDDBAA2",
+      "address": "1150 Roberts Blvd NW, Kennesaw, UNITED STATES,GA,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "agenda": 3
+      },
+      "zipCode": "30144",
+      "latitude ": "34.0050825",
+      "longitude": "-84.5850713"
+    },
+    {
+      "account_Name": "Automated Logic Houston",
+      "accountID": "0018000001FlOzQAAV",
+      "amount": "0",
+      "address": "4107 New W Dr, Pasadena, UNITED STATES,TX,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "77507-1883",
+      "latitude ": "29.6501227",
+      "longitude": "-95.0931487"
+    },
+    {
+      "account_Name": "Automatic Data Processing Inc",
+      "accountID": "0013400001JpTpkAAF",
+      "amount": "0",
+      "address": "1 Adp Blvd Ste 1, Roseland, UNITED STATES,NJ,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "07068-1786",
+      "latitude ": "40.8155834",
+      "longitude": "-74.3100114"
+    },
+    {
+      "account_Name": "Aviation Technicalerts Services Inc",
+      "accountID": "0018000000hoRTEAA2",
+      "address": "3100 112th Street SW, Everett, UNITED STATES,WA,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "98204",
+      "latitude ": 47.8971632,
+      "longitude": -122.2790725
+    },
+    {
+      "account_Name": "Aviation Technicalerts Services Inc",
+      "accountID": "0013400001JxH4CAAV",
+      "amount": "0",
+      "address": "3121 109th St SW, Everett, UNITED STATES,WA,null",
+      "accountDetails": {
+        "amount": "2423",
+        "alerts": 1,
+        "opportunities": 32,
+        "support_request": 2,
+        "insights": "0",
+        "agenda": 3
+      },
+      "zipCode": "98204-1318"
+    }
+    
+  ]
+};
+
+var newAccountJSON = [];
+
+var length = data.success.length;
+
+for(var i=0 ; i<length ; i++)
+{
+  var newValue = {
+
+    "accName" : data.success[i].account_Name ? data.success[i].account_Name : "NA" , 
+    "accID" : data.success[i].accountID ? data.success[i].accountID : "NA" , 
+    "accDetails" : data.success[i].accountDetails ? 
+    {
+      "amt" : data.success[i].accountDetails.amount ? parseInt(data.success[i].accountDetails.amount) : "NA" , 
+      "alrt" : data.success[i].accountDetails.alerts ? data.success[i].accountDetails.alerts : "NA" , 
+      "opt" : data.success[i].accountDetails.opportunities ? data.success[i].accountDetails.opportunities : "NA" , 
+      "sR" : data.success[i].accountDetails.support_request ? data.success[i].accountDetails.support_request : "NA" , 
+      "iS" : data.success[i].accountDetails.insights ? data.success[i].accountDetails.insights : "NA" , 
+      "agd" : data.success[i].accountDetails.agenda ? data.success[i].accountDetails.agenda : "NA" 
+    } : "{}" , 
+    "add" : data.success[i].address ? data.success[i].address : "NA" , 
+    "loc" : 
+    {
+      "zC" : data.success[i].zipCode ? data.success[i].zipCode : "NA" , 
+      "lat" : data.success[i].latitude ? parseFloat(parseFloat(data.success[i].latitude).toFixed(7)) : "NA" , 
+      "long" : data.success[i].longitude ? parseFloat(parseFloat(data.success[i].longitude).toFixed(7)) : "NA"  
+    }
+    
+  };
+
+  newAccountJSON.push(newValue);
+}
+
+console.log(JSON.stringify(newAccountJSON));
